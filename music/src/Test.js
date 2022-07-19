af2bf3e26343761ac2c9094bc531bf8bd329f3ae
AcePizza <stianchrbakke@gmail.com>
2022-07-19 14:02:48 +0200

    removed some imports

--- music/src/Test.js
+++ music/src/Test.js
@@ -3,8 +3,6 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
-import Image from "react-bootstrap/Image";
 import LoadingPleaseWait from "./LoadingPleaseWait";
 
 function Test({ albumData }) {
