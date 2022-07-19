00bfea323ce7e6c995d00d511ae8b7a7a02dae83
AcePizza <stianchrbakke@gmail.com>
2022-07-19 20:39:20 +0200

    added some API template literals for the fetch

--- music/src/GetData.js
+++ music/src/GetData.js
@@ -2,13 +2,20 @@ import React, { useEffect, useState } from "react";
 import MainNavbar from "./components/MainNavbar";
 import MainPageFilter from "./components/MainPageFilter";
 import Test from "./Test";
+import DisplayCard from "./components/DisplayCard";
+import LoadingPleaseWait from "./components/LoadingPleaseWait";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const API_URL = `https://api.discogs.com/database/search?${API_KEY}`;
 
 function GetData() {
   const [albumData, setAlbumData] = useState();
 
   const fetchAlbumData = async () => {
     try {
-      const response = await fetch("https://api.discogs.com/artists/1?");
+      const response = await fetch(
+        `https://api.discogs.com/database/search?${API_KEY}`
+      );
       const results = await response.json();
       setAlbumData(results);
     } catch (error) {
@@ -24,7 +31,7 @@ function GetData() {
     <div>
       <MainNavbar />
       <MainPageFilter />
-      <Test albumData={albumData} />
+      {!albumData ? <LoadingPleaseWait /> : <p>Didnt happen</p>}
     </div>
   );
 }
