7199dc34d4334795e74b970874b8c26fe1fbfff2
AcePizza <stianchrbakke@gmail.com>
2022-07-19 12:41:23 +0200

    added spinner, filter, navbar

--- music/src/Test.js
+++ music/src/Test.js
@@ -3,7 +3,9 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
+import LoadingPleaseWait from "./LoadingPleaseWait";
 
 function Test({ albumData }) {
   console.log("albumData", albumData);
@@ -11,20 +13,48 @@ function Test({ albumData }) {
   return (
     <div>
       <Container>
-        <Row>
-          <Col>
-            <Card style={{ borderWidth: "2px" }}>
-              <Card.Body>
-                {!albumData ? "some error" : albumData.name}
-              </Card.Body>
-            </Card>
-            <Card>
-              <Card.Body>
-                <Image className="rounded" src="holder.js/100px180"></Image>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
+        <Card style={{ borderWidth: "2px", padding }}>
+          <Card.Body>
+            <Row>
+              <Col>
+                {!albumData ? (
+                  <LoadingPleaseWait />
+                ) : (
+                  <Card style={{ borderWidth: "0px", width: "18rem" }}>
+                    <Card.Img variant="top" src="./No_image_available.png" />
+                    <Card.Body>
+                      <Card.Title>Card Title</Card.Title>
+                    </Card.Body>
+                  </Card>
+                )}
+              </Col>
+              <Col>
+                {!albumData ? (
+                  <LoadingPleaseWait />
+                ) : (
+                  <Card style={{ borderWidth: "0px", width: "18rem" }}>
+                    <Card.Img variant="top" src="./No_image_available.png" />
+                    <Card.Body>
+                      <Card.Title>Card Title</Card.Title>
+                    </Card.Body>
+                  </Card>
+                )}
+              </Col>
+              <Col>
+                {!albumData ? (
+                  <LoadingPleaseWait />
+                ) : (
+                  <Card style={{ borderWidth: "0px", width: "18rem" }}>
+                    <Card.Img variant="top" src="./No_image_available.png" />
+                    <Card.Body>
+                      <Card.Title>Card Title</Card.Title>
+                    </Card.Body>
+                  </Card>
+                )}
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
       </Container>
     </div>
   );
