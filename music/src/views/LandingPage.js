936c8779cba4eaf55cdfed44c51dad143bd8c170
AcePizza <stianchrbakke@gmail.com>
2022-08-03 13:09:30 +0200

    added some images

--- music/src/views/LandingPage.js
+++ music/src/views/LandingPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import Col from "react-bootstrap/esm/Col";
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
+import re4Merchant from "../assets/85-854627_got-somethin-that-might-interest-ya-merchant.png";
 
 function LandingPage() {
   return (
@@ -9,6 +10,14 @@ function LandingPage() {
       <Row>
         <Col>
           <h4>Welcome stranger!</h4>
+          <img
+            src={re4Merchant}
+            style={{
+              height: "100%",
+              width: "100%",
+              borderRadius: "5px",
+            }}
+          ></img>
         </Col>
       </Row>
     </Container>
--- music/src/views/NothingMuch.js
+++ music/src/views/NothingMuch.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
+import Container from "react-bootstrap/esm/Container";
+import mapImage from "../assets/image-asset.jpeg";
 
 function NothingMuch() {
   return (
-    <div>
-      <p>Here there be dragons</p>
-    </div>
+    <Container>
+      <h4>Here there be dragons</h4>
+      <Row>
+        <Col>
+          {" "}
+          <img
+            src={mapImage}
+            style={{
+              height: "200px",
+              width: "250px",
+              borderRadius: "5px",
+            }}
+          ></img>
+        </Col>{" "}
+        <a href="/home">Click here to return to home</a>
+      </Row>
+    </Container>
   );
 }
 
