ae206cfa27cf0ac3547b9c6c6a858d5030f4948c
AcePizza <stianchrbakke@gmail.com>
2022-08-04 13:34:28 +0200

    contructed object with ID for use with the delete

--- music/src/views/ChatPage.js
+++ music/src/views/ChatPage.js
@@ -17,20 +17,36 @@ function ChatPage() {
 
   // Checkes the messages which are stored in the DB
   const checkMessagesFromDB = async () => {
-    const messageArray = [];
+    let messageArray = [];
     const querySnapshot = await getDocs(collection(db, "chat"));
     querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data()}`);
-      messageArray.push(doc.data());
+      // splitting data and time into smaller parts
+      const data = doc.data();
+      const timestamp = new Date(data.time.seconds * 1000);
+
+      // Constructing a new object for use later
+      const arr = {
+        id: doc.id,
+        author: data.author,
+        text: data.text,
+        time: `${timestamp.getDate()}/${timestamp.getMonth()} ${timestamp.getHours()}:${timestamp.getMinutes()}`,
+      };
+
+      // Pushing the element into an object
+      messageArray.push(arr);
     });
+    // Setting the state variable to the constructed object
     setMessages(messageArray);
   };
 
+  // The new message constructor
   const onChangeMessageHandler = (e) => {
     setNewMessage(e.target.value);
   };
 
+  // adds a new message to the database
   const onClickMessageHandler = async () => {
+    // Check to see if there is a use loggedin (Maybe this is not usefull)
     whoIsUser ? <LoadingPleaseWait /> : signedInUser();
 
     const docRef = await addDoc(collection(db, "chat"), {
@@ -40,13 +56,23 @@ function ChatPage() {
     });
   };
 
+  // This handles the deletion of a message in the database
+  const onCloseMessageHandeler = (messageid) => {
+    console.log("this will close the message");
+    console.log("The id from the emement", messageid);
+  };
+
   useEffect(() => {
     checkMessagesFromDB();
   }, []);
 
-  messages
-    ? console.log(new Date(messages[0].time.seconds * 1000))
-    : console.log("this is not true");
+  useEffect(() => {
+    onCloseMessageHandeler();
+  }, [onCloseMessageHandeler]);
+
+  // messages
+  //   ? console.log(new Date(messages[0].time.seconds * 1000))
+  //   : console.log("this is not true");
 
   return (
     <Container>
@@ -57,10 +83,10 @@ function ChatPage() {
           messages.map((message, index) => {
             return (
               <Col key={index}>
-                <Toast>
+                <Toast onClose={onCloseMessageHandeler(message.id)}>
                   <Toast.Header>
                     <strong className="me-auto">{message.author}</strong>
-                    <small>somethings</small>
+                    <small>{message.time}</small>
                   </Toast.Header>
                   <Toast.Body>{message.text}</Toast.Body>
                 </Toast>
