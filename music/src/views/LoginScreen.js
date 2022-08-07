6d97b7383fd3e8040160617fd4a45c5a7d05b1ca
AcePizza <stianchrbakke@gmail.com>
2022-08-07 21:02:48 +0200

    put a loader on the login screen

--- music/src/views/LoginScreen.js
+++ music/src/views/LoginScreen.js
@@ -1,9 +1,11 @@
+import { async } from "@firebase/util";
 import React, { useContext, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/esm/Container";
 import Form from "react-bootstrap/Form";
 import { useNavigate } from "react-router-dom";
 import { LoginStoreContext } from "../components/context/loginContext";
+import LoadingPleaseWait from "../components/LoadingPleaseWait";
 
 function LoginScreen() {
   // the context data (Test). The Value from this is not set globally
@@ -14,14 +16,12 @@ function LoginScreen() {
   const [password, setPassword] = useState("");
   const { login } = useContext(LoginStoreContext);
 
-  console.log("isUserLoggedIn : ", isUserLoggedIn);
-
-  const loginEmailInputHandeler = (e) => {
-    setEmail(e.target.value);
+  const loginEmailInputHandeler = (evt) => {
+    setEmail(evt.target.value);
   };
 
-  const loginPasswordInputHandeler = (e) => {
-    setPassword(e.target.value);
+  const loginPasswordInputHandeler = (evt) => {
+    setPassword(evt.target.value);
   };
 
   const loginNewUserHandeler = () => {
@@ -32,48 +32,57 @@ function LoginScreen() {
     logout();
   };
 
-  return (
-    <Container>
-      {isUserLoggedIn ? (
-        <Button variant="danger" onClick={logOutUser}>
-          Log out
-        </Button>
-      ) : (
-        <Form>
-          <h4>Please enter your login credentials</h4>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Enter email"
-              value={email}
-              onChange={loginEmailInputHandeler}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={loginPasswordInputHandeler}
-            />
-          </Form.Group>
-          <Button variant="primary" onClick={loginNewUserHandeler}>
-            Login
-          </Button>{" "}
-          <Button
-            variant="info"
-            onClick={() => {
-              redirectTo("/registeruser");
-            }}
-          >
-            Create User
+  const isSomeoneLoggedIn = () => {
+    switch (isUserLoggedIn) {
+      case undefined:
+        return <LoadingPleaseWait />;
+      case false:
+        return (
+          <Form>
+            <h4>Please enter your login credentials</h4>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={loginEmailInputHandeler}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={loginPasswordInputHandeler}
+              />
+            </Form.Group>
+            <Button variant="primary" onClick={loginNewUserHandeler}>
+              Login
+            </Button>{" "}
+            <Button
+              variant="info"
+              onClick={() => {
+                redirectTo("/registeruser");
+              }}
+            >
+              Create User
+            </Button>
+          </Form>
+        );
+      case true:
+        return (
+          <Button variant="danger" onClick={logOutUser}>
+            Log out
           </Button>
-        </Form>
-      )}
-    </Container>
-  );
+        );
+    }
+  };
+
+  console.log("is the user logged in", isUserLoggedIn);
+
+  return <Container>{isSomeoneLoggedIn()}</Container>;
 }
 
 export default LoginScreen;
