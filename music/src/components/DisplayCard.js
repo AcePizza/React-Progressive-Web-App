b4ed72a6ba5dbf339b0dce096c5dd03dee793709
AcePizza <stianchrbakke@gmail.com>
2022-08-08 14:27:11 +0200

    added like button and input handler

--- music/src/components/DisplayCard.js
+++ music/src/components/DisplayCard.js
@@ -1,12 +1,20 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import { Row } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import ToggleButton from "react-bootstrap/ToggleButton";
+import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
 import { Link } from "react-router-dom";
 import { LoginStoreContext } from "../components/context/loginContext";
+import { LikeContext } from "./context/likeContext";
 
 function DisplayCard({ artist, index }) {
   const artistName = artist;
   const { isUserLoggedIn } = useContext(LoginStoreContext);
+  const { buttonInput, setButtonInput } = useContext(LikeContext);
+
+  const handleButtonChange = () => {};
 
   return (
     <Col key={index}>
@@ -18,9 +26,24 @@ function DisplayCard({ artist, index }) {
       >
         <Card.Img className="fluid" variant="top" src={artist.cover_image} />
         <Card.Body>
-          <Card.Title>
-            <Link to={`details/${artistName.id}`}>{artistName.title}</Link>
-          </Card.Title>
+          <Row>
+            <Col>
+              <Card.Title>
+                <Link to={`details/${artistName.id}`}>{artistName.title}</Link>
+              </Card.Title>
+            </Col>
+            <Col>
+              <ToggleButtonGroup
+                type="checkbox"
+                value={buttonInput}
+                onChange={handleButtonChange}
+              >
+                <ToggleButton id="tbg-btn-1" value={1}>
+                  Like
+                </ToggleButton>
+              </ToggleButtonGroup>
+            </Col>
+          </Row>
         </Card.Body>
       </Card>
     </Col>
